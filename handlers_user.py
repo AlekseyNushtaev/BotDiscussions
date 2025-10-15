7b1bd5b69a8f7f804e45a5a0750c68a6f3bd46ce
Aleksei Nushtaev <nushtaev2110@gmail.com>
2025-10-15 21:57:45 +0500

    add policy

--- handlers_user.py
+++ handlers_user.py
@@ -92,8 +92,8 @@ async def ask_question(callback: CallbackQuery, state: FSMContext):
                    callback.from_user.first_name,
                    callback.from_user.last_name)
     await callback.message.answer_photo(photo=FSInputFile('vopros.jpg'),
-                                        caption="❓ Пожалуйста, задайте ваш вопрос.\n"
-                                                "*Отправляя сообщение Вы соглашаетесь с [Политикой конфиденциальности](https://odobrino.ru/policy)",
+                                        caption="❓ Пожалуйста, задайте ваш вопрос\.\n"
+                                                "*Отправляя сообщение Вы соглашаетесь с [Политикой конфиденциальности](https://odobrino\.ru/policy)",
                                         parse_mode="MarkdownV2")
     await state.set_state(QuestionState.waiting_for_question)
     await callback.answer()
